9b8d1a47a883ff985144d883ec0b38c09d3a287d
rinInwza007 <02854@sw-phayao.ac.th>
2025-09-05 23:36:39 +0700

    v1

---
+++ Myweb/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-3xl font-bold text-blue-600">
+        🚀 React + Vite + Tailwind v4 ทำงานแล้ว!
+      </h1>
+    </div>
+  )
+}
+
+export default App
